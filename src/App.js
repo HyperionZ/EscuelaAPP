19f086f12ba60c2818774b05cbc3c2ef3cfe6e1c
Cristhian_DEV <hyperionz20@hotmail.com>
2019-09-29 08:03:30 -0500

    formulario

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Header,
+  Icon,
+  Divider,
+  Container,
+  Table,
+  TableCell,
+  Button
+} from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header as="h2" icon textAlign="center">
+          <Icon name="book" circular />
+          <Header.Content>EscuelaAPP</Header.Content>
+        </Header>
+        <Divider />
+        <Container>
+          <Table fixed>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Nombre</Table.HeaderCell>
+                <Table.HeaderCell>Edad</Table.HeaderCell>
+                <Table.HeaderCell>Curso Actual</Table.HeaderCell>
+                <Table.HeaderCell>Acciones</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>John</Table.Cell>
+                <Table.Cell>22</Table.Cell>
+                <Table.Cell>Grado de desarrollo de Aplicaciones Web</Table.Cell>
+                <Table.Cell>
+                  <Button animated>
+                    <Button.Content visible>
+                      <Icon name="eye" />
+                    </Button.Content>
+                    <Button.Content hidden>
+                      <Icon name="arrow right" />
+                    </Button.Content>
+                  </Button>
+                  <Button color="green" animated>
+                    <Button.Content visible>
+                      <Icon name="pencil" />
+                    </Button.Content>
+                    <Button.Content hidden>
+                      <Icon name="arrow right" />
+                    </Button.Content>
+                  </Button>
+                  <Button color="red" animated>
+                    <Button.Content visible>
+                      <Icon name="trash" />
+                    </Button.Content>
+                    <Button.Content hidden>
+                      <Icon name="arrow right" />
+                    </Button.Content>
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
